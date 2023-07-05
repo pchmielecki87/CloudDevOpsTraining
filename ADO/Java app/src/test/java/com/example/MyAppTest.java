9e8215dd68cea652a62932be85fb9389ef1ef6f6
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2023-07-05 14:48:43 +0200

    cleaning

--- ADO/Java app/src/test/java/com/example/MyAppTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-public class HelloWorldTest {
-    @Test
-    public void testHelloWorld() {
-        assertEquals("Hello, World!", HelloWorld.getHelloWorldMessage());
-    }
-}
